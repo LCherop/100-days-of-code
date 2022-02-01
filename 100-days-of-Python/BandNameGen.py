55d4e8e76bb700068e3103724996cdb1550e8410
Laura Cherop <77297998+LCherop@users.noreply.github.com>
2022-02-02 00:14:46 +0300

    Add files via upload

---
+++ 100-days-of-Python/BandNameGen.py
@@ -0,0 +1,6 @@
+print("Hello, welcome to your band name generator!")
+city = input("What is the name of the city you grew up in: \n")
+petname = input("What is your pet's name:\n ")
+print("Your band name could be "+ city +" "+ petname)
+
+
