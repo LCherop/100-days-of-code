233836bd6ebcdaed5d8d95687ab1d9720b93dca8
LCherop <lauracherop27@gmail.com>
2023-01-26 18:59:40 +0300

    day 21

---
+++ 100-days-of-Python/day_18/main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle,Screen
+timz = Turtle()
+
+timz.forward(100)
+timz.left(90)
+timz.forward(100)
+timz.left(90)
+timz.forward(100)
+timz.left(90)
+timz.forward(100)
+
+
+screen = Screen()
+screen.exitonclick()
